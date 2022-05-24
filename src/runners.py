a8a171ce55f5ef756a39172e92010f1b34d13baa
ahmed <ahmedshah1494@gmail.com>
2022-05-23 21:39:45 -0400

    added runner and trainer for warming up act opt lr

--- src/runners.py
+++ src/runners.py
@@ -4,19 +4,29 @@ from mllib.runners.base_runners import BaseRunner
 from mllib.runners.configs import BaseExperimentConfig
 from mllib.tasks.base_tasks import AbstractTask
 
-from trainers import AdversarialTrainer, AdversarialParams
+from trainers import ActivityOptimizationParams, AdversarialTrainer, AdversarialParams, ConsistentActivationModelAdversarialTrainer
 
 @define(slots=False)
 class AdversarialExperimentConfig(BaseExperimentConfig):
     adv_config: AdversarialParams = AdversarialParams()
 
-class AdversarialExperimentRunner(BaseRunner):
-    def __init__(self, task: AbstractTask) -> None:
-        super().__init__(task)
+@define(slots=False)
+class ConsistentActivationAdversarialExperimentConfig(AdversarialExperimentConfig):
+    act_opt_config: ActivityOptimizationParams = ActivityOptimizationParams()
 
+class AdversarialExperimentRunner(BaseRunner):
     def create_trainer(self):
         trainer_params = self.create_trainer_params()
         adv_trainer_params = AdversarialTrainer.get_params()
         adv_trainer_params.trainer_params = trainer_params
         adv_trainer_params.adversarial_params = self.task.get_experiment_params().adv_config
+        self.trainer = adv_trainer_params.cls(adv_trainer_params)
+
+class ConsistentActivationAdversarialExperimentRunner(AdversarialExperimentRunner):
+    def create_trainer(self):
+        trainer_params = self.create_trainer_params()
+        adv_trainer_params = ConsistentActivationModelAdversarialTrainer.get_params()
+        adv_trainer_params.trainer_params = trainer_params
+        adv_trainer_params.adversarial_params = self.task.get_experiment_params().adv_config
+        adv_trainer_params.act_opt_params = self.task.get_experiment_params().act_opt_config
         self.trainer = adv_trainer_params.cls(adv_trainer_params)
--- src/trainers.py
+++ src/trainers.py
@@ -1,3 +1,4 @@
+from enum import Enum, auto
 import os
 from typing import List, Type, Union
 from attrs import define
@@ -8,10 +9,9 @@ from numpy import iterable
 import torch
 import numpy as np
 
-from models import ConsistentActivationClassifier
-from mllib.adversarial.attacks import AbstractAttackConfig, AttackParamFactory
+from mllib.adversarial.attacks import AbstractAttackConfig
+from models import ConsistencyOptimizationMixin
 from pruning import PruningMixin
-from trainer_utils import _make_adv_datset, make_dataloader
 from utils import aggregate_dicts, merge_iterables_in_dict, write_json, write_pickle
 
 @define(slots=False)
@@ -20,10 +20,11 @@ class AdversarialParams:
     testing_attack_params: List[AbstractAttackConfig] = [None]
 
 class AdversarialTrainer(_Trainer, PruningMixin):
-    @define(slots=False)
     class AdversarialTrainerParams(BaseParameters):
-        trainer_params: Type[_Trainer.TrainerParams] = _Trainer.TrainerParams(None)
-        adversarial_params: Type[AdversarialParams] = AdversarialParams
+        def __init__(self, cls):
+            super().__init__(cls)
+            self.trainer_params: Type[_Trainer.TrainerParams] = _Trainer.TrainerParams(None)
+            self.adversarial_params: Type[AdversarialParams] = AdversarialParams
 
     @classmethod
     def get_params(cls):
@@ -33,7 +34,6 @@ class AdversarialTrainer(_Trainer, PruningMixin):
         super().__init__(params.trainer_params)
         self.params = params
         self.training_adv_attack = self._maybe_get_attacks(params.adversarial_params.training_attack_params)
-
         self.testing_adv_attacks = self._maybe_get_attacks(params.adversarial_params.testing_attack_params)
 
     def _get_attack_from_params(self, p: AbstractAttackConfig):
@@ -67,11 +67,11 @@ class AdversarialTrainer(_Trainer, PruningMixin):
         test_acc = {}
         for eps, atk in zip(test_eps, self.testing_adv_attacks):
             x,y = self._maybe_attack_batch(batch, atk)
-            y = y.detach().cpu()
 
             logits, loss = self._get_outputs_and_loss(x, y)
             logits = logits.detach().cpu()
-
+            
+            y = y.detach().cpu()
             acc, _ = compute_accuracy(logits, y)
 
             preds = get_preds_from_logits(logits)
@@ -122,11 +122,9 @@ class AdversarialTrainer(_Trainer, PruningMixin):
         test_outputs, test_metrics = self.test_loop(post_loop_fn=self.test_epoch_end)
         print('test metrics:')
         print(test_metrics)
-
-        _, train_metrics = self._batch_loop(self.train_step, self.train_loader, 0, logging=False)
+        with torch.no_grad():
+            _, train_metrics = self._batch_loop(self.train_step, self.train_loader, 0, logging=False)
         train_metrics = aggregate_dicts(train_metrics)
-        # test_metrics = aggregate_dicts(test_metrics)
-        # test_accs = {float(k.split('_')[-1]): v for k,v in test_metrics.items() if k.startswith('test_acc_')}
 
         self.save_training_logs(train_metrics['train_accuracy'], test_outputs['test_acc'])
         self.save_data_and_preds(test_outputs['preds'], test_outputs['labels'], test_outputs['inputs'])
@@ -137,3 +135,55 @@ class AdversarialTrainer(_Trainer, PruningMixin):
                 self.logger.add_scalars(k, v, global_step=step)
             elif not iterable(v):
                 self.logger.add_scalar(k, v, global_step=step)
+
+class ActivityOptimizationSchedule(Enum):
+    CONST = auto()
+    GEOM = auto()
+    LINEAR = auto()
+
+@define(slots=False)
+class ActivityOptimizationParams:
+    act_opt_lr_warmup_schedule: Type[ActivityOptimizationSchedule] = ActivityOptimizationSchedule.CONST
+    init_act_opt_lr: float = 1e-2
+    num_warmup_epochs: int = 5
+
+class ConsistentActivationModelAdversarialTrainer(AdversarialTrainer):
+    class ConsistentActivationModelAdversarialTrainerParams(BaseParameters):
+        def __init__(self, cls):
+            super().__init__(cls)
+            self.trainer_params: Type[_Trainer.TrainerParams] = _Trainer.TrainerParams(None)
+            self.adversarial_params: Type[AdversarialParams] = AdversarialParams
+            self.act_opt_params: ActivityOptimizationParams = ActivityOptimizationParams()
+    
+    @classmethod
+    def get_params(cls):
+        return cls.ConsistentActivationModelAdversarialTrainerParams(cls)
+
+    def __init__(self, params: ConsistentActivationModelAdversarialTrainerParams):
+        super().__init__(params)
+        self.params = params
+        self._load_max_act_opt_lrs()
+
+    def _load_max_act_opt_lrs(self):
+        self.max_act_opt_lrs = {}
+        for n, m in self.model.named_modules():
+            if isinstance(m, ConsistencyOptimizationMixin):
+                self.max_act_opt_lrs[n] = m.act_opt_step_size
+
+    def _update_act_opt_lrs(self, epoch_idx: int):
+        if (epoch_idx < self.params.act_opt_params.num_warmup_epochs) and self.params.act_opt_params.act_opt_lr_warmup_schedule != ActivityOptimizationSchedule.CONST:
+            for n,m in self.model.named_modules():
+                if n in self.max_act_opt_lrs:
+                    if self.params.act_opt_params.act_opt_lr_warmup_schedule == ActivityOptimizationSchedule.GEOM:
+                        m.act_opt_step_size = np.geomspace(self.params.act_opt_params.init_act_opt_lr, 
+                                                            self.max_act_opt_lrs[n], 
+                                                            self.params.act_opt_params.num_warmup_epochs)[epoch_idx]
+                    if self.params.act_opt_params.act_opt_lr_warmup_schedule == ActivityOptimizationSchedule.LINEAR:
+                        m.act_opt_step_size = np.linspace(self.params.act_opt_params.init_act_opt_lr, 
+                                                            self.max_act_opt_lrs[n], 
+                                                            self.params.act_opt_params.num_warmup_epochs)[epoch_idx]
+                    print(n, m.act_opt_step_size)
+
+    def train_loop(self, epoch_idx, post_loop_fn=None):
+        self._update_act_opt_lrs(epoch_idx)
+        return super().train_loop(epoch_idx, post_loop_fn)
