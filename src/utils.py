141cdd20eb7f1e17d04304f2b54ddd33fdc20841
ahmed <ahmedshah1494@gmail.com>
2022-08-30 20:49:13 -0400

    - added function to get eps value from attack name - _load_logs now loads adv metrics from attack battery - added function to compute area under curve - added function to recursively update dictionaries

--- src/utils.py
+++ src/utils.py
@@ -73,24 +73,40 @@ def write_pickle(d, path):
     with open(path, 'wb') as f:
         pickle.dump(d, f)
 
-def _load_logs(logdir, model_filename='model_ckp.json', metrics_filename='metrics.json', 
+def get_eps_from_logdict_key(s):
+    if s.replace('.','',1).isdigit():
+        eps = float(s)
+        atkname = ''
+    else:
+        atkname, eps = s.split('-')
+        eps = float(eps)
+    return atkname, eps
+
+def _load_logs(logdir, model_filename='model_ckp.json', metrics_filename='metrics.json',
                 state_hist_filename='state_dict_hist.json', data_filename='data_and_preds.pkl',
-                args_filename='task.pkl'):
+                adv_battery_data_filename='adv_data_and_preds.pkl', adv_metrics_filename='adv_metrics.json',
+                adv_succ_filename='adv_succ.json', args_filename='task.pkl'):
+    print(logdir)
     metrics_path = os.path.join(logdir, metrics_filename)
     if os.path.exists(metrics_path):
         metrics = load_json(metrics_path)
     else:
-        metrics = aggregate_metrics(logdir)
+        metrics = aggregate_metrics(logdir, metrics_filename)
+        if len(metrics) == 0:
+            metrics = aggregate_metrics(logdir, adv_metrics_filename)
     args_path = os.path.join(logdir, args_filename)
     if os.path.exists(args_path):
         args = load_pickle(args_path)
     else:
         args = None
-    metrics['test_accs'] = {float(k):v for k,v in metrics['test_accs'].items()}
+    # metrics['test_accs'] = {k:v for k,v in metrics['test_accs'].items()}
     lazy_model_ckps = []
     lazy_state_hists = []
     lazy_data_and_preds = []
+    lazy_adv_data_and_preds = []
     model_metrics = []
+    model_adv_metrics = []
+    model_adv_succ = []
     model_paths = []
     for root, _, files in os.walk(logdir):
         model_files = [f for f in files if f.startswith('model') and f.endswith('.pt')]
@@ -103,19 +119,33 @@ def _load_logs(logdir, model_filename='model_ckp.json', metrics_filename='metric
         if data_filename in files:
             data_fp = os.path.join(root, data_filename)
             lazy_data_and_preds.append(lazy_load_pickle(data_fp))
+        if adv_battery_data_filename in files:
+            data_fp = os.path.join(root, adv_battery_data_filename)
+            lazy_adv_data_and_preds.append(lazy_load_pickle(data_fp))
+        if adv_metrics_filename in files:
+            am_fp = os.path.join(root, adv_metrics_filename)
+            model_adv_metrics.append(load_json(am_fp))
+        if adv_succ_filename in files:
+            as_fp = os.path.join(root, adv_succ_filename)
+            model_adv_succ.append(load_json(as_fp))
         if metrics_filename in files and (root != logdir):
             m_fp = os.path.join(root, metrics_filename)
             model_metrics.append(load_json(m_fp))
             model_paths.append(root)
-        if args is None and (args_filename in files):
-            args_path = os.path.join(root, args_filename)
-            args = args = load_pickle(args_path)
+        # if args is None and (args_filename in files):
+        #     args_path = os.path.join(root, args_filename)
+        #     args = load_pickle(args_path)
+    if len(model_metrics) == 0:
+        model_metrics = model_adv_metrics
     log_dict = {
         'metrics': metrics,
         'models': lazy_model_ckps,
         'model_metrics': model_metrics,
         'model_paths': model_paths,
+        'model_adv_metrics': model_adv_metrics,
+        'model_adv_succ': model_adv_succ,
         'data_and_preds': lazy_data_and_preds,
+        'adv_data_and_preds': lazy_adv_data_and_preds,
         'args': args
     }
     return log_dict
@@ -139,11 +169,38 @@ def load_logs(logdirs, labels):
             logdicts[label] = _load_logs(logdir)
     return logdicts
 
-def get_model_checkpoint_dirs(d):
+def _compute_area_under_curve(x, y):
+    x = np.array(x)
+    y = np.array(y)
+    sorted_idx = sorted(range(len(x)), key=lambda i: x[i])
+    x = x[sorted_idx]
+    y = y[sorted_idx]
+    total_area = 0
+    for i in range(1, len(x)):
+        h = x[i] - x[i-1]
+        assert h >= 0
+        a = y[i]
+        b = y[i-1]
+        area = h*(a+b)/2
+        total_area += area
+    return total_area
+
+def get_model_checkpoint_paths(d):
     model_ckp_dirs = []
     for root, dirs, files in os.walk(d):
         model_files = [f for f in files if f.startswith('model') and f.endswith('.pt')]
         if len(model_files) > 0:
-            model_files = model_files[0]
-            model_ckp_dirs.append(os.path.dirname(root))
+            model_file = model_files[0]
+            model_ckp_dirs.append(os.path.join(root, model_file))
     return model_ckp_dirs
+
+def recursive_dict_update(dsrc, dtgt):
+    for ksrc, vsrc in dsrc.items():
+        if ksrc in dtgt:
+            vtgt = dtgt[ksrc]
+            if isinstance(vtgt, dict) and isinstance(vsrc, dict):
+                recursive_dict_update(vsrc, vtgt)
+            else:
+                dtgt[ksrc] = vsrc
+        else:
+            dtgt[ksrc] = vsrc
