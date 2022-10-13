284475f8c1308182459f0945c92b8b3356a7e2df
ahmed <ahmedshah1494@gmail.com>
2022-10-13 14:58:34 -0400

    - Removed the requirement of metadata - using pqdm to speed up

--- src/convert_pytorch_imagefolder_to_npz_dataset.py
+++ src/convert_pytorch_imagefolder_to_npz_dataset.py
@@ -3,16 +3,17 @@ import torch
 import torchvision
 import numpy as np
 import json
-from tqdm import tqdm
+from pqdm.processes import pqdm
 from multiprocessing import Pool
+from task_utils import logdir_root
 
-DATASET = 'imagenet-100'
-RESIZE = 64
+DATASET = 'ecoset-100'
+RESIZE = 320
 
-outfolder = f'/home/mshah1/workhorse3/{DATASET}/bin/'
+outfolder = f'{logdir_root}/{DATASET}/bin/'
 if RESIZE > 0:
     outfolder = os.path.join(outfolder, str(RESIZE))
-datafolder = f'/home/mshah1/workhorse3/{DATASET}/'
+datafolder = f'{logdir_root}/{DATASET}/'
 
 if RESIZE > 0:
     transforms = torchvision.transforms.Compose([
@@ -40,7 +41,12 @@ if not os.path.exists(outfolder):
 
 print('creating datasets...')
 train_dataset = torchvision.datasets.ImageFolder(os.path.join(datafolder, 'train'), transform=transforms)
-test_dataset = torchvision.datasets.ImageFolder(os.path.join(datafolder, 'val'), transform=transforms)
+if os.path.exists(os.path.join(datafolder, 'test')):
+    test_dataset = torchvision.datasets.ImageFolder(os.path.join(datafolder, 'test'), transform=transforms)
+    val_dataset = torchvision.datasets.ImageFolder(os.path.join(datafolder, 'val'), transform=transforms)
+else:
+    test_dataset = torchvision.datasets.ImageFolder(os.path.join(datafolder, 'val'), transform=transforms)
+
 assert test_dataset.class_to_idx == train_dataset.class_to_idx
 
 print('creating idx2word')
@@ -49,16 +55,19 @@ if os.path.exists(os.path.join(datafolder, 'words.txt')) and os.path.exists(os.p
     wnids = np.loadtxt(os.path.join(datafolder, 'wnids.txt'), dtype=str, delimiter='\t')
     wnid2word = {i: n for i,n in zip(wnid2word[:, 0], wnid2word[:,1]) if i in wnids}
     idx2word = {test_dataset.class_to_idx[wi]: w for wi,w in wnid2word.items()}
-    with open(os.path.join(outfolder, 'idx2word.json'), 'w') as f:
-        json.dump(idx2word, f)
+else:
+    idx2word = train_dataset.class_to_idx
+with open(os.path.join(outfolder, 'idx2word.json'), 'w') as f:
+    json.dump(idx2word, f)
 
 def save_dataset(dataset, outfile):
     # for x, y in tqdm.tq(dataset):
     #     xa = np.array(x)
     #     X.append(xa)
     #     Y.append(y)
-    with Pool(8) as p:
-        data = p.map(dataset.__getitem__, range(len(dataset)))
+    # with Pool(8) as p:
+    #     data = p.map(dataset.__getitem__, range(len(dataset)))
+    data = pqdm(range(len(dataset)), dataset.__getitem__, n_jobs=16)
     X, Y = zip(*data)
     X = [np.array(x) for x in X]
     X = np.stack(X, 0)
@@ -66,8 +75,11 @@ def save_dataset(dataset, outfile):
     print(X.shape, Y.shape)
     np.savez_compressed(os.path.join(outfolder, outfile), X=X, Y=Y)
 
-print('saving test dataset...')
-save_dataset(test_dataset, 'test.pkl')
+# print('saving test dataset...')
+# save_dataset(test_dataset, 'test.pkl')
+# if 'val_dataset' in locals():
+#     print('saving val dataset...')
+#     save_dataset(val_dataset, 'val.pkl')
 print('saving train dataset...')
 save_dataset(train_dataset, 'train.pkl')
     
