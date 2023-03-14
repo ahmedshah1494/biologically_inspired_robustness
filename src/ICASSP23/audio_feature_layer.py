941a2bd2e16cdbdbd0415e43f4812b715007134c
ahmed <ahmedshah1494@gmail.com>
2023-03-14 14:26:58 -0400

    added melspec, logmelspec and specaug layers

--- src/ICASSP23/audio_feature_layer.py
+++ src/ICASSP23/audio_feature_layer.py
@@ -1,8 +1,10 @@
 from turtle import forward
+from typing import Optional, Tuple
 from attrs import define
 from mllib.models.base_models import AbstractModel
 from mllib.param import BaseParameters
-from torchaudio.transforms import MFCC, Resample
+from torchaudio.transforms import MFCC, Resample, MelSpectrogram, FrequencyMasking, TimeMasking, TimeStretch
+import numpy as np
 import torch
 
 class ResamplingLayer(AbstractModel):
@@ -52,6 +54,79 @@ class MFCCExtractionLayer(AbstractModel):
 
     def forward(self, x):
         x = self.mfcc(x)
+        return x
+    
+    def compute_loss(self, x, y, return_logits=True):
+        logits = self.forward(x)
+        loss = torch.zeros((x.shape[0],), device=x.device)
+        if return_logits:
+            return logits, loss
+        else:
+            return loss
+
+class MelSpectrogramExtractionLayer(AbstractModel):
+    @define(slots=False)
+    class ModelParams(BaseParameters):
+        sample_rate: int = 16000
+        n_fft: int = 400
+        win_length: Optional[int] = None
+        hop_length: Optional[int] = None
+        f_min: float = 0.0
+        f_max: Optional[float] = None
+        pad: int = 0
+        n_mels: int = 128
+        power: float = 2.0
+        normalized: bool = False
+        center: bool = True
+        pad_mode: str = 'reflect'
+        onesided: bool = True
+        norm: Optional[str] = None
+        mel_scale: str = 'htk'
+    
+    def __init__(self, params: ModelParams) -> None:
+        super().__init__(params)
+        self.melspec = MelSpectrogram(**(params.asdict(filter=lambda a,v: a.name != 'cls')))
+    
+    def forward(self, x):
+        return self.melspec(x)
+
+    def compute_loss(self, x, y, return_logits=True):
+        logits = self.forward(x)
+        loss = torch.zeros((x.shape[0],), device=x.device)
+        if return_logits:
+            return logits, loss
+        else:
+            return loss
+
+class LogMelSpectrogramExtractionLayer(MelSpectrogramExtractionLayer):
+    def forward(self,x):
+        melspec = super().forward(x)+1e-5
+        logmelspec = torch.log(melspec)
+        return logmelspec
+
+class SpecAugmentLayer(AbstractModel):
+    @define(slots=False)
+    class ModelParams(BaseParameters):
+        freq_mask_param: int = None
+        time_mask_param: int = None
+        time_stretch_range: Tuple[float, float] = None
+        hop_length: int = None
+        n_freq: int = 201,
+    
+    def __init__(self, params: ModelParams) -> None:
+        super().__init__(params)
+        self.params = params
+        self.freq_mask = FrequencyMasking(self.params.freq_mask_param)
+        self.time_mask = TimeMasking(self.params.time_mask_param)
+        if self.params.time_stretch_range:
+            self.time_stretch = TimeStretch(self.params.hop_length, self.params.n_freq)
+    
+    def forward(self, x):
+        if self.training:
+            x = self.freq_mask(x)
+            x = self.time_mask(x)
+            if self.params.time_stretch_range:
+                x = self.time_stretch(x, np.random.uniform(*(self.params.time_stretch_range)))
         return x
     
     def compute_loss(self, x, y, return_logits=True):
