849dd9cc06c5de7dc0a02ac872c13dac018a90ff
ahmed <ahmedshah1494@gmail.com>
2023-04-04 09:55:44 -0400

    added tasks for DoG blur filter

---
+++ src/ICLR22/noisy_DoG_blur.py
@@ -0,0 +1,84 @@
+import torchvision
+from adversarialML.biologically_inspired_models.src.mlp_mixer_models import NormalizationLayer
+from adversarialML.biologically_inspired_models.src.models import (
+    CommonModelParams, GeneralClassifier, SequentialLayers, XResNet34, XResNet18,)
+from adversarialML.biologically_inspired_models.src.retina_blur2 import RetinaBlurFilter as RBlur2
+from adversarialML.biologically_inspired_models.src.retina_preproc import (GaussianNoiseLayer, RetinDoGBlurFilter)
+from adversarialML.biologically_inspired_models.src.trainers import LightningAdversarialTrainer
+from mllib.optimizers.configs import (AdamOptimizerConfig,
+                                      CosineAnnealingWarmRestartsConfig,
+                                      CyclicLRConfig, LinearLRConfig,
+                                      ReduceLROnPlateauConfig,
+                                      SequentialLRConfig, OneCycleLRConfig, SGDOptimizerConfig)
+from mllib.runners.configs import BaseExperimentConfig, TrainingParams
+from mllib.tasks.base_tasks import AbstractTask
+from torch import nn
+from adversarialML.biologically_inspired_models.src.task_utils import *
+
+class Ecoset10NoisyRetinaDoGBlurS2500WRandomScalesCyclicLR1e_1RandAugmentXResNet2x18(AbstractTask):
+    imgs_size = 224
+    input_size = [3, imgs_size, imgs_size]
+    widen_factor = 2
+    noise_std = 0.25
+    def get_dataset_params(self) :
+        p = get_ecoset10_params(train_transforms=[
+                torchvision.transforms.Resize(self.imgs_size),
+                torchvision.transforms.RandomCrop(self.imgs_size),
+                torchvision.transforms.RandomHorizontalFlip(),
+                torchvision.transforms.RandAugment(magnitude=15)
+            ],
+            test_transforms=[
+                torchvision.transforms.Resize(self.imgs_size),
+                torchvision.transforms.CenterCrop(self.imgs_size),
+            ])
+        return p
+
+    def get_model_params(self):
+        rnoise_p = GaussianNoiseLayer.ModelParams(GaussianNoiseLayer, std=self.noise_std)
+        rblur_p = RetinDoGBlurFilter.ModelParams(RetinDoGBlurFilter, self.input_size, batch_size=32, cone_std=0.12, 
+                                                rod_std=0.09, max_rod_density=0.12, view_scale='random_uniform',
+                                                use_1d_gkernels=True)
+        rp = SequentialLayers.ModelParams(SequentialLayers, [rnoise_p, rblur_p], CommonModelParams(self.input_size, activation=nn.Identity))
+        resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams(self.input_size, 10), num_classes=10,
+                                            normalization_layer_params=NormalizationLayer.get_params(),
+                                            widen_factor=self.widen_factor)
+        p = GeneralClassifier.ModelParams(GeneralClassifier, self.input_size, rp, resnet_p)
+        return p
+
+    def get_experiment_params(self) -> BaseExperimentConfig:
+        nepochs = 60
+        return BaseExperimentConfig(
+            LightningAdversarialTrainer.TrainerParams(LightningAdversarialTrainer,
+                TrainingParams(logdir=LOGDIR, nepochs=nepochs, early_stop_patience=50, tracked_metric='val_accuracy',
+                    tracking_mode='max', scheduler_step_after_epoch=False
+                )
+            ),
+            SGDOptimizerConfig(lr=0.2, weight_decay=5e-4, momentum=0.9, nesterov=True),
+            OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=375, pct_start=0.1, anneal_strategy='linear'),
+            logdir=LOGDIR, batch_size=128
+        )
+
+class Ecoset10NoisyRetinaDoGBlurFovR8S2500WRandomScalesCyclicLR1e_1RandAugmentXResNet2x18(Ecoset10NoisyRetinaDoGBlurS2500WRandomScalesCyclicLR1e_1RandAugmentXResNet2x18):
+    def get_model_params(self):
+        rnoise_p = GaussianNoiseLayer.ModelParams(GaussianNoiseLayer, std=self.noise_std)
+        rblur_p = RetinDoGBlurFilter.ModelParams(RetinDoGBlurFilter, self.input_size, batch_size=32, cone_std=0.12, 
+                                                rod_std=0.09, max_rod_density=0.12, view_scale='random_uniform',
+                                                use_1d_gkernels=True, min_bincount=self.imgs_size//50)
+        rp = SequentialLayers.ModelParams(SequentialLayers, [rnoise_p, rblur_p], CommonModelParams(self.input_size, activation=nn.Identity))
+        resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams(self.input_size, 10), num_classes=10,
+                                            normalization_layer_params=NormalizationLayer.get_params(),
+                                            widen_factor=self.widen_factor)
+        p = GeneralClassifier.ModelParams(GeneralClassifier, self.input_size, rp, resnet_p)
+        return p
+    
+class Ecoset10NeuronNoisyRetinaDoGBlurS2500WRandomScalesCyclicLR1e_1RandAugmentXResNet2x18(Ecoset10NoisyRetinaDoGBlurS2500WRandomScalesCyclicLR1e_1RandAugmentXResNet2x18):
+    def get_model_params(self):
+        rnoise_p = GaussianNoiseLayer.ModelParams(GaussianNoiseLayer, std=1., neuronal_noise=True)
+        rblur_p = RetinDoGBlurFilter.ModelParams(RetinDoGBlurFilter, self.input_size, batch_size=32, cone_std=0.12, 
+                                                rod_std=0.09, max_rod_density=0.12, view_scale='random_uniform')
+        rp = SequentialLayers.ModelParams(SequentialLayers, [rnoise_p, rblur_p], CommonModelParams(self.input_size, activation=nn.Identity))
+        resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams(self.input_size, 10), num_classes=10,
+                                            normalization_layer_params=NormalizationLayer.get_params(),
+                                            widen_factor=self.widen_factor)
+        p = GeneralClassifier.ModelParams(GeneralClassifier, self.input_size, rp, resnet_p)
+        return p
