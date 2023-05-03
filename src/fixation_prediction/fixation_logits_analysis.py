daee2142f6bc95d190ff7bd2a1dcc9165d93a367
ahmed <ahmedshah1494@gmail.com>
2023-05-03 08:54:33 -0400

    added code for fixation logit combination analysis

--- src/fixation_prediction/fixation_logits_analysis.py
+++ src/fixation_prediction/fixation_logits_analysis.py
@@ -25,13 +25,14 @@ import matplotlib.pyplot as plt
 ds = FixationPointDataset(
     '/share/workhorse3/mshah1/ecoset-10',
     'val',
-    # '/share/workhorse3/mshah1/biologically_inspired_models/iclr22_logs/ecoset10-0.0/Ecoset10NoisyRetinaBlurS2500WRandomScalesCyclicLR1e_1RandAugmentXResNet2x18/0/fixation_logits/49',
-    '/share/workhorse3/mshah1/biologically_inspired_models/iclr22_logs/ecoset10-0.0/Ecoset10NoisyRetinaBlurS2500WRandomScalesCyclicLR1e_1RandAugmentXResNet2x18/0/fixation_logits/0.004/16/',
+    '/share/workhorse3/mshah1/biologically_inspired_models/iclr22_logs/ecoset10-0.0/Ecoset10NoisyRetinaBlurS2500WRandomScalesCyclicLR1e_1RandAugmentXResNet2x18/0/fixation_logits/49',
+    # '/share/workhorse3/mshah1/biologically_inspired_models/iclr22_logs/ecoset10-0.0/Ecoset10NoisyRetinaBlurS2500WRandomScalesCyclicLR1e_1RandAugmentXResNet2x18/0/fixation_logits/0.004/16/',
     # '/share/workhorse3/mshah1/ecoset-100/',
     # 'val',
     # '/share/workhorse3/mshah1/biologically_inspired_models/iclr22_logs/ecoset100_folder-0.0/ecoset100_folder-0.0/Ecoset100NoisyRetinaBlurWRandomScalesCyclicLRRandAugmentXResNet2x18/0/fixation_logits/16/',
-    # '/share/workhorse3/mshah1/ecoset/val',
-    # '/share/workhorse3/mshah1/biologically_inspired_models/iclr22_logs/ecoset-0.0/EcosetNoisyRetinaBlurWRandomScalesCyclicLRRandAugmentXResNet2x18/0/fixation_logits/49/val/',
+    # '/share/workhorse3/mshah1/ecoset/',
+    # 'val',
+    # '/share/workhorse3/mshah1/biologically_inspired_models/iclr22_logs/ecoset-0.0/EcosetNoisyRetinaBlurWRandomScalesCyclicLRRandAugmentXResNet2x18/0/fixation_logits/49/',
     transform=torchvision.transforms.Compose([torchvision.transforms.Resize(224),
                 torchvision.transforms.CenterCrop(224),
                 torchvision.transforms.ToTensor()]), fixation_target='logit'
@@ -47,14 +48,38 @@ print(imgs.shape, flogits.shape, Y.shape)
 def average_logit_pred(logits):
     return np.argmax(logits.mean(1), 1)
 
+def top5_average_logit_pred(logits):
+    return np.argsort(logits.mean(1), 1)[:, -5:]
+
 def soft_voting_pred(logits):
     return np.argmax(softmax(logits, 2).sum(1), 1)
 
+def top5_soft_voting_pred(logits):
+    return np.argsort(softmax(logits, 2).sum(1), 1)[:, -5:]
+
+# def vote_pred(logits):
+#     return np.array([x[np.argmax(np.unique(x, return_counts=True)[1])] for x in np.argmax(logits, -1)])
+
 def vote_pred(logits):
-    return np.array([x[np.argmax(np.unique(x, return_counts=True)[1])] for x in np.argmax(logits, -1)])
+    # votes = np.array([np.unique(fpreds, return_counts=True)[1] for fpreds in logits.argmax(-1)])
+    preds = []
+    for fpreds in logits.argmax(-1):
+        labels, counts = np.unique(fpreds, return_counts=True)
+        pred = labels[counts.argmax()]
+        preds.append(pred)
+    return np.array(preds)
+
+
+def top5_vote_pred(logits):
+    return np.array([x[np.argmax(np.unique(x, return_counts=True)[1])] for x in np.argsort(logits, -1)[..., -5:].reshape(logits.shape[0], -1)])
 
 def is_correct(preds, labels):
-    return (preds == labels)
+    while len(labels.shape) < len(preds.shape):
+        labels = np.expand_dims(labels, -1)
+    is_c = (preds == labels)
+    if len(is_c.shape) == 2:
+        is_c = is_c.any(1)
+    return is_c
 
 def accuracy(logits, labels, pred_fn):
     preds = pred_fn(logits)
@@ -64,6 +89,26 @@ def accuracy(logits, labels, pred_fn):
 def oracle_accuracy(logits, labels):
     preds = np.argmax(logits, -1)
     is_c = (preds == labels.reshape(-1, 1)).any(1)
+    return is_c.astype(float).mean()
+
+def all_correct_accuracy(logits, labels):
+    preds = np.argmax(logits, -1)
+    is_c = (preds == labels.reshape(-1, 1)).all(1)
+    return is_c.astype(float).mean()
+
+def top5_all_correct_accuracy(logits, labels):
+    top5preds = np.argsort(logits, -1)[...,-5:]
+    is_c = np.zeros((top5preds.shape[0],top5preds.shape[1])).astype(bool)
+    for i in range(5):
+        is_c |= (top5preds[...,i] == labels.reshape(-1, 1))
+    is_c = is_c.all(1)
+    return is_c.astype(float).mean()
+
+def oracle_top5_accuracy(logits, labels):
+    top5preds = np.argsort(logits, -1)[...,-5:]
+    is_c = np.zeros((top5preds.shape[0],)).astype(bool)
+    for i in range(5):
+        is_c |= (top5preds[...,i] == labels.reshape(-1, 1)).any(1)
     return is_c.astype(float).mean()
 
 def get_5point_idxs(N):
@@ -104,6 +149,88 @@ def get_topK_logits2(logits, k):
     topk_logits = np.stack(topk_logits, 1)
     return topk_logits
 
+def get_topK_logit_combinations(logits, K):
+    sum_logits = 0
+    selected_logits = []
+    fxpts = []
+    for k in range(K):
+        sum_logits = logits+sum_logits
+        probs = softmax(sum_logits, 2)
+        top2idx = np.argpartition(probs, logits.shape[2] - 2, 2)[:, :, -2:]
+        # print(top2idx.shape, probs[0], top2idx[0])
+        top2margin = np.stack([p[range(len(idxs)),idxs[:,1]]-p[range(len(idxs)),idxs[:,0]] for p,idxs in zip(probs, top2idx)],0)
+        if len(fxpts) > 0:
+            for pts in fxpts:
+                top2margin[range(len(top2margin)), pts] *= -np.inf
+        # print(top2margin.shape, probs[0], top2margin[0])
+        max_margin_idxs = np.argmax(top2margin, 1)
+        fxpts.append(max_margin_idxs)
+        selected_logits.append(logits[range(len(logits)), max_margin_idxs])
+        sum_logits = np.expand_dims(sum_logits[range(len(logits)), max_margin_idxs], 1)
+    #     print(max_margin_idxs[0])
+    #     print(sum_logits[0])
+    #     print(logits[0, max_margin_idxs[0]])
+    #     print(probs[0, max_margin_idxs[0]])
+    # print([p[0] for p in fxpts])
+    selected_logits = np.stack(selected_logits, 1)
+    # print(selected_logits.shape)
+    # print(softmax(np.cumsum(selected_logits[12], 0), 1))
+    # print(sum_logits[12])
+    return selected_logits
+
+def get_topK_logit_oracle_combinations(logits, K, Y):
+    sum_logits = 0
+    selected_logits = []
+    fxpts = []
+    for k in range(K):
+        sum_logits = logits+sum_logits
+        probs = softmax(sum_logits, 2)
+        clsprobs = np.stack([p[:, y] for p,y in zip(probs, Y)], 0)
+        for p,y in zip(probs, Y):
+            p[:, y] = 0
+        topidx = np.argmax(probs, 2)
+        # print(clsprobs[0], probs[0], topidx[0])
+        # print(top2idx.shape, probs[0], top2idx[0])
+        top2margin = np.stack([cp-p[range(len(idx)),idx] for cp,p,idx, y in zip(clsprobs, probs, topidx, Y)],0)
+        if len(fxpts) > 0:
+            for pts in fxpts:
+                top2margin[range(len(top2margin)), pts] = -np.inf
+        # print(top2margin.shape, probs[0], top2margin[0])
+        max_margin_idxs = np.argmax(top2margin, 1)
+        fxpts.append(max_margin_idxs)
+        selected_logits.append(logits[range(len(logits)), max_margin_idxs])
+        sum_logits = np.expand_dims(sum_logits[range(len(logits)), max_margin_idxs], 1)
+        # print(max_margin_idxs[0], Y[0], top2margin[0])
+        # print(sum_logits[0])
+        # print(logits[0, max_margin_idxs[0]])
+        # print(probs[0, max_margin_idxs[0]])
+    # print([p[0] for p in fxpts])
+    selected_logits = np.stack(selected_logits, 1)
+    return selected_logits
+
+def get_topK_logit_oracle_combinations2(logits, K, Y):
+    from itertools import combinations
+    selected_logits = []
+    locidxs = []
+    ncorrect = 0
+    for lg,y in zip(logits,Y):
+        combs = combinations(range(logits.shape[1]), K)
+        for i,c in enumerate(combs):
+            c = list(c)
+            is_correct = np.argmax(lg[c].mean(0)) == y
+            if is_correct:
+                ncorrect += 1
+                selected_logits.append(lg[c])
+                break
+        locidxs.append(i)
+        if len(locidxs) > len(selected_logits):
+            selected_logits.append(lg[c])
+    selected_logits = np.stack(selected_logits, 0)
+    acc = (selected_logits.mean(1).argmax(1) == Y).astype(float).mean()
+    print(logits.shape, selected_logits.shape, ncorrect/len(logits), acc)
+    print(min(locidxs), np.mean(locidxs), np.median(locidxs), max(locidxs))
+    return selected_logits
+
 
 def make_features(flogits):
     X = flogits
@@ -115,19 +242,37 @@ def make_features(flogits):
 npts = flogits.shape[1]
 five_points = get_5point_idxs(npts)
 flogits_top5 = get_topK_logits2(flogits, 2)
+flogits_top5comb = get_topK_logit_combinations(flogits, 5)
+flogits_oracletop5comb = get_topK_logit_oracle_combinations(flogits, 5, Y)
+# flogits_top1comb_ex = get_topK_logit_oracle_combinations2(flogits, 1, Y)
+flogits_oracletop2comb_ex = get_topK_logit_oracle_combinations2(flogits, 2, Y)
+# flogits_top3comb_ex = get_topK_logit_oracle_combinations2(flogits, 3, Y)
 # print(five_points)
 # exit()
 flogits_5f = flogits[:, five_points]
-print('mean logits 5pt:', accuracy(flogits_5f, Y, average_logit_pred))
-print('mean logits top-5pt:', accuracy(flogits_top5, Y, average_logit_pred))
-print('mean logits:', accuracy(flogits, Y, average_logit_pred))
-print('voting 5pt:', accuracy(flogits_5f, Y, vote_pred))
-print('voting top-5pt:', accuracy(flogits_top5, Y, soft_voting_pred))
-print('voting:', accuracy(flogits, Y, vote_pred))
-print('soft voting:', accuracy(flogits, Y, soft_voting_pred))
-print('oracle-5pt:', oracle_accuracy(flogits_5f, Y))
-print('oracle-top-5pt:', oracle_accuracy(flogits_top5, Y))
-print(f'oracle-{npts}pt:', oracle_accuracy(flogits, Y))
+print('all points:', all_correct_accuracy(flogits, Y), top5_all_correct_accuracy(flogits, Y))
+print('center:', accuracy(flogits_5f[:, [2]], Y, average_logit_pred), accuracy(flogits_5f[:, [2]], Y, top5_average_logit_pred))
+print('mean logits:', accuracy(flogits, Y, average_logit_pred), accuracy(flogits, Y, top5_average_logit_pred))
+print('mean logits 5pt:', accuracy(flogits_5f, Y, average_logit_pred), accuracy(flogits_5f, Y, top5_average_logit_pred))
+print('mean logits top-5pt:', accuracy(flogits_top5, Y, average_logit_pred), accuracy(flogits_top5, Y, top5_average_logit_pred))
+print('mean logits top-5comb:', accuracy(flogits_top5comb, Y, average_logit_pred), accuracy(flogits_top5comb, Y, top5_average_logit_pred))
+print('mean logits oracle-top-5comb:', accuracy(flogits_oracletop5comb, Y, average_logit_pred), accuracy(flogits_oracletop5comb, Y, top5_average_logit_pred))
+# print('mean logits oracle-top-1 exhaustive comb:', accuracy(flogits_top1comb_ex, Y, average_logit_pred), accuracy(flogits_top1comb_ex, Y, top5_average_logit_pred))
+print('mean logits oracle-top-2 exhaustive comb:', accuracy(flogits_oracletop2comb_ex, Y, average_logit_pred), accuracy(flogits_oracletop2comb_ex, Y, top5_average_logit_pred))
+# print('mean logits oracle-top-3 exhaustive comb:', accuracy(flogits_top3comb_ex, Y, average_logit_pred), accuracy(flogits_top3comb_ex, Y, top5_average_logit_pred))
+print('voting 5pt:', accuracy(flogits_5f, Y, vote_pred), accuracy(flogits_5f, Y, top5_vote_pred))
+print('voting:', accuracy(flogits, Y, vote_pred), accuracy(flogits, Y, top5_vote_pred))
+print('soft voting:', accuracy(flogits, Y, soft_voting_pred), accuracy(flogits, Y, top5_soft_voting_pred))
+print('soft voting 5pt:', accuracy(flogits_5f, Y, soft_voting_pred), accuracy(flogits_5f, Y, top5_soft_voting_pred))
+print('soft voting top-5pt:', accuracy(flogits_top5, Y, soft_voting_pred), accuracy(flogits_top5, Y, top5_soft_voting_pred))
+print('soft voting top-5comb:', accuracy(flogits_top5comb, Y, soft_voting_pred), accuracy(flogits_top5comb, Y, top5_soft_voting_pred))
+print('soft voting oracle-top-5comb:', accuracy(flogits_oracletop5comb, Y, soft_voting_pred), accuracy(flogits_oracletop5comb, Y, top5_soft_voting_pred))
+print('oracle-5pt:', oracle_accuracy(flogits_5f, Y), oracle_top5_accuracy(flogits_5f, Y))
+print('oracle top-5comb:', oracle_accuracy(flogits_top5comb, Y), oracle_top5_accuracy(flogits_top5comb, Y))
+print('oracle oracle-top-5comb:', oracle_accuracy(flogits_oracletop5comb, Y), oracle_top5_accuracy(flogits_oracletop5comb, Y))
+print('oracle oracle-top-2 exhaustive comb:', oracle_accuracy(flogits_oracletop2comb_ex, Y), oracle_top5_accuracy(flogits_oracletop2comb_ex, Y))
+print('oracle top-5pt:', oracle_accuracy(flogits_top5, Y), oracle_top5_accuracy(flogits_top5, Y))
+print(f'oracle {npts}pt:', oracle_accuracy(flogits, Y), oracle_top5_accuracy(flogits, Y))
 exit()
 
 # def predict_fixation():
