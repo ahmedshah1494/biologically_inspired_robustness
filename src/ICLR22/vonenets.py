651abef5669ce120824f2c3571efda10725dde94
ahmed <ahmedshah1494@gmail.com>
2023-05-19 11:27:12 -0400

    added normalization and removed conv-bn-relu-maxpool

--- src/ICLR22/vonenets.py
+++ src/ICLR22/vonenets.py
@@ -24,7 +24,17 @@ class Ecoset100VOneBlockCyclicLRRandAugmentXResNet2x18(AbstractTask):
     input_size = [3, imgs_size, imgs_size]
     widen_factor = 2
     def get_dataset_params(self) :
-        p = get_ecoset100shards_params(num_test=10, train_transforms=[
+        # p = get_ecoset100shards_params(num_test=10, train_transforms=[
+        #         torchvision.transforms.Resize(self.imgs_size),
+        #         torchvision.transforms.RandomCrop(self.imgs_size),
+        #         torchvision.transforms.RandomHorizontalFlip(),
+        #         torchvision.transforms.RandAugment(magnitude=15)
+        #     ],
+        #     test_transforms=[
+        #         torchvision.transforms.Resize(self.imgs_size),
+        #         torchvision.transforms.CenterCrop(self.imgs_size),
+        #     ])
+        p = get_ecoset100folder_params(num_train=500000, train_transforms=[
                 torchvision.transforms.Resize(self.imgs_size),
                 torchvision.transforms.RandomCrop(self.imgs_size),
                 torchvision.transforms.RandomHorizontalFlip(),
@@ -34,13 +44,18 @@ class Ecoset100VOneBlockCyclicLRRandAugmentXResNet2x18(AbstractTask):
                 torchvision.transforms.Resize(self.imgs_size),
                 torchvision.transforms.CenterCrop(self.imgs_size),
             ])
+        # Pointing to a folder with only the test set, and some dummy train and val data. 
+        # Use this on workhorse to avoid delay due to slow NFS.
+        p.datafolder = f'{logdir_root}/ecoset-100/eval_dataset_dir'
         return p
 
     def get_model_params(self):
         vonep = VOneBlock.ModelParams(VOneBlock, add_deterministic_noise_during_inference=True)
+        normp = NormalizationLayer.ModelParams(NormalizationLayer, [0.5]*3, [0.5]*3)
+        v1p = SequentialLayers.ModelParams(SequentialLayers, [normp, vonep], CommonModelParams(self.input_size, activation=nn.Identity))
         resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams([64, self.imgs_size, self.imgs_size], 100), num_classes=100,
-                                            widen_factor=self.widen_factor, stem_sizes=(64,64,64))
-        p = GeneralClassifier.ModelParams(GeneralClassifier, self.input_size, vonep, resnet_p)
+                                            widen_factor=self.widen_factor, stem_sizes=(64,64,64), drop_layers=[0,3])
+        p = GeneralClassifier.ModelParams(GeneralClassifier, self.input_size, v1p, resnet_p)
         return p
 
     def get_experiment_params(self) -> BaseExperimentConfig:
@@ -155,10 +170,12 @@ class Ecoset10VOneBlockCyclicLRRandAugmentXResNet2x18(AbstractTask):
         return p
 
     def get_model_params(self):
-        vonep = VOneBlock.ModelParams(VOneBlock, dropout_p=0.5, add_deterministic_noise_during_inference=True)
-        resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams([64, self.imgs_size, self.imgs_size], 10, dropout_p=0.5), num_classes=10,
-                                            widen_factor=self.widen_factor, stem_sizes=(64,64,64))
-        p = GeneralClassifier.ModelParams(GeneralClassifier, self.input_size, vonep, resnet_p)
+        vonep = VOneBlock.ModelParams(VOneBlock, add_deterministic_noise_during_inference=True)
+        normp = NormalizationLayer.ModelParams(NormalizationLayer, [0.5]*3, [0.5]*3)
+        v1p = SequentialLayers.ModelParams(SequentialLayers, [normp, vonep], CommonModelParams(self.input_size, activation=nn.Identity))
+        resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams([64, self.imgs_size, self.imgs_size], 10), num_classes=10,
+                                            widen_factor=self.widen_factor, stem_sizes=(64,64,64), drop_layers=[0, 3])
+        p = GeneralClassifier.ModelParams(GeneralClassifier, self.input_size, v1p, resnet_p)
         return p
 
     def get_experiment_params(self) -> BaseExperimentConfig:
@@ -193,13 +210,14 @@ class Ecoset10VOneBlockWNoisyRBlurCyclicLRRandAugmentXResNet2x18(AbstractTask):
         return p
 
     def get_model_params(self):
-        vonep = VOneBlock.ModelParams(VOneBlock, visual_degrees=60, add_deterministic_noise_during_inference=True)
+        vonep = VOneBlock.ModelParams(VOneBlock, add_deterministic_noise_during_inference=True)
+        normp = NormalizationLayer.ModelParams(NormalizationLayer, [0.5]*3, [0.5]*3)
         resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams([64, self.imgs_size, self.imgs_size], 10), num_classes=10,
-                                            widen_factor=self.widen_factor, stem_sizes=(64,64,64))
+                                            widen_factor=self.widen_factor, stem_sizes=(64,64,64), drop_layers=[0, 3])
         rnoise_p = GaussianNoiseLayer.ModelParams(GaussianNoiseLayer, std=self.noise_std)
         rblur_p = RetinaBlurFilter.ModelParams(RetinaBlurFilter, self.input_size, batch_size=32, cone_std=0.12, 
                                                 rod_std=0.09, max_rod_density=0.12, view_scale='random_uniform', use_1d_gkernels=True)
-        rp = SequentialLayers.ModelParams(SequentialLayers, [rnoise_p, rblur_p, vonep], CommonModelParams(self.input_size, activation=nn.Identity))
+        rp = SequentialLayers.ModelParams(SequentialLayers, [rnoise_p, rblur_p, normp, vonep], CommonModelParams(self.input_size, activation=nn.Identity))
         p = GeneralClassifier.ModelParams(GeneralClassifier, self.input_size, rp, resnet_p)
         return p
 
@@ -297,4 +315,94 @@ class Ecoset10VOneBlockWRBlur2CyclicLRRandAugmentXResNet2x18(AbstractTask):
             SGDOptimizerConfig(lr=0.2, weight_decay=5e-4, momentum=0.9, nesterov=True),
             OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=375, pct_start=0.1, anneal_strategy='linear'),
             logdir=LOGDIR, batch_size=128
+        )
+    
+class ImagenetVOneBlockCyclicLRXResNet2x18(AbstractTask):
+    imgs_size = 224
+    input_size = [3, imgs_size, imgs_size]
+    widen_factor = 2
+    def get_dataset_params(self):
+        p = get_imagenet_folder_params(num_train=1_275_000, train_transforms=[
+                torchvision.transforms.Resize(self.imgs_size),
+                torchvision.transforms.RandomCrop(self.imgs_size),
+                torchvision.transforms.RandomHorizontalFlip(),
+            ],
+            test_transforms=[
+                torchvision.transforms.Resize(self.imgs_size),
+                torchvision.transforms.CenterCrop(self.imgs_size),
+            ])
+        # Pointing to a folder with only the test set, and some dummy train and val data. 
+        # Use this on workhorse to avoid delay due to slow NFS.
+        # p.datafolder = f'{logdir_root}/imagenet/eval_dataset_dir'
+        return p
+
+    def get_model_params(self):
+        vonep = VOneBlock.ModelParams(VOneBlock, add_deterministic_noise_during_inference=True)
+        normp = NormalizationLayer.ModelParams(NormalizationLayer, [0.5]*3, [0.5]*3)
+        v1p = SequentialLayers.ModelParams(SequentialLayers, [normp, vonep], CommonModelParams(self.input_size, activation=nn.Identity))
+        resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams([64, self.imgs_size, self.imgs_size], 1000), num_classes=1000,
+                                            widen_factor=self.widen_factor, stem_sizes=(64,64,64), drop_layers=[0, 3])
+        p = GeneralClassifier.ModelParams(GeneralClassifier, self.input_size, v1p, resnet_p)
+        return p
+
+    def get_experiment_params(self) -> BaseExperimentConfig:
+        nepochs = 25
+        return BaseExperimentConfig(
+            LightningAdversarialTrainer.TrainerParams(LightningAdversarialTrainer,
+                TrainingParams(logdir=LOGDIR, nepochs=nepochs, early_stop_patience=50, tracked_metric='val_accuracy',
+                    tracking_mode='max', scheduler_step_after_epoch=False
+                )
+            ),
+            SGDOptimizerConfig(lr=0.2, weight_decay=5e-4, momentum=0.9, nesterov=True),
+            # OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=1839, pct_start=0.05, anneal_strategy='linear'),
+            # OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=5632, pct_start=0.1, anneal_strategy='linear'),
+            OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=4916, pct_start=0.1, anneal_strategy='cos', div_factor=10, three_phase=True),
+            logdir=LOGDIR, batch_size=64 # 4GPUS 256 batch
+        )
+    
+class EcosetVOneBlockCyclicLRXResNet2x18(AbstractTask):
+    imgs_size = 224
+    input_size = [3, imgs_size, imgs_size]
+    widen_factor = 2
+    def get_dataset_params(self) :
+        p = get_ecoset_params(train_transforms=[
+                torchvision.transforms.Resize(self.imgs_size),
+                torchvision.transforms.RandomCrop(self.imgs_size),
+                torchvision.transforms.RandomHorizontalFlip(),
+            ],
+            test_transforms=[
+                torchvision.transforms.Resize(self.imgs_size),
+                torchvision.transforms.CenterCrop(self.imgs_size),
+            ])
+        # Pointing to a folder with only the test set, and some dummy train and val data. 
+        # Use this on workhorse to avoid delay due to slow NFS.
+        p = get_ecoset_folder_params(test_transforms=[
+                torchvision.transforms.Resize(self.imgs_size),
+                torchvision.transforms.CenterCrop(self.imgs_size),
+            ])
+        p.datafolder = f'{logdir_root}/ecoset/eval_dataset_dir'
+        return p
+
+    def get_model_params(self):
+        vonep = VOneBlock.ModelParams(VOneBlock, add_deterministic_noise_during_inference=True)
+        normp = NormalizationLayer.ModelParams(NormalizationLayer, [0.5]*3, [0.5]*3)
+        v1p = SequentialLayers.ModelParams(SequentialLayers, [normp, vonep], CommonModelParams(self.input_size, activation=nn.Identity))
+        resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams([64, self.imgs_size, self.imgs_size], 1000), num_classes=1000,
+                                            widen_factor=self.widen_factor, stem_sizes=(64,64,64), drop_layers=[0, 3])
+        p = GeneralClassifier.ModelParams(GeneralClassifier, self.input_size, v1p, resnet_p)
+        return p
+
+    def get_experiment_params(self) -> BaseExperimentConfig:
+        nepochs = 25
+        return BaseExperimentConfig(
+            LightningAdversarialTrainer.TrainerParams(LightningAdversarialTrainer,
+                TrainingParams(logdir=LOGDIR, nepochs=nepochs, early_stop_patience=50, tracked_metric='val_accuracy',
+                    tracking_mode='max', scheduler_step_after_epoch=False
+                )
+            ),
+            SGDOptimizerConfig(lr=0.2, weight_decay=5e-4, momentum=0.9, nesterov=True),
+            # OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=1839, pct_start=0.05, anneal_strategy='linear'),
+            # OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=5632, pct_start=0.1, anneal_strategy='linear'),
+            OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=5632, pct_start=0.1, anneal_strategy='cos', div_factor=10, three_phase=True),
+            logdir=LOGDIR, batch_size=64 # 4GPUS 256 batch
         )
