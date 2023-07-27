33e3c7e41bb59155cc69a108e67ae95eb793b0a4
ahmed <ahmedshah1494@gmail.com>
2023-07-27 10:37:40 -0700

    updated bionorm code

--- src/bio_receptive_fields/bionorm.py
+++ src/bio_receptive_fields/bionorm.py
@@ -4,12 +4,17 @@ from mllib.models.base_models import AbstractModel
 from mllib.param import BaseParameters
 from attrs import define
 
-class BioNorm(nn.Module):
+class _BioNorm(nn.Module):
     def __init__(
             self,
             num_features:int,
             kernel_size:int,
             affine:bool=True,
+            m: int = 2,
+            n: int = 1,
+            p: int = 1/2,
+            sigma: int = 1e-2,
+            center: bool = False,
             device:torch.device=None,
             dtype:torch.dtype=None
         ) -> None:
@@ -18,7 +23,7 @@ class BioNorm(nn.Module):
             (a mini-batch of 2D inputs with additional channel dimension). This operation exponentiates
             and divides the value at each spatial coordinate by the sum of (exponentiated) neighboring
             values. The neighborhood is defined by the kernel_size.
-                R[:, c] = \gamma\frac{X[:, c]^n}{\sigma^n + K^2 AvgPool2d(X, kernel_size=K, stride=1)} + \beta
+                R[:, c] = \gamma\frac{X[:, c]^n}{\sigma^n + (K^2 AvgPool2d(X, kernel_size=K, stride=1))^\frac{n}{p}} + \beta
             where \sigma, n, \gamma and \beta are trainable parameters.
             Args:
                 - num_features (int): number of channels in the input
@@ -30,10 +35,13 @@ class BioNorm(nn.Module):
         self.num_features = num_features
         self.kernel_size = kernel_size
         self.affine = affine
+        self.n = n
+        self.m = m
+        self.p = p
+        self.center = center
 
-        self.sigma = nn.parameter.Parameter(torch.empty(num_features, **factory_kwargs), requires_grad=False)
-        self.pow = nn.parameter.Parameter(torch.empty(num_features, **factory_kwargs), requires_grad=False)
-        self.sum_kernel = nn.parameter.Parameter(torch.empty(self.num_features, 1, self.kernel_size, self.kernel_size), requires_grad=False)
+        self.sigma = sigma
+        self.sum_kernel = nn.parameter.Parameter(torch.empty(1, self.num_features, self.kernel_size, self.kernel_size), requires_grad=False)
         if self.affine:
             self.weight = nn.parameter.Parameter(torch.empty(num_features, **factory_kwargs))
             self.bias = nn.parameter.Parameter(torch.empty(num_features, **factory_kwargs))
@@ -51,29 +59,69 @@ class BioNorm(nn.Module):
         if self.affine:
             nn.init.ones_(self.weight)
             nn.init.zeros_(self.bias)
-        nn.init.constant_(self.pow, 2)
-        nn.init.constant_(self.sigma, 1)
-        nn.init.constant_(self.sum_kernel, 1/(self.kernel_size**2))
+        nn.init.constant_(self.sum_kernel, 1/(self.num_features*self.kernel_size**2))
 
     def forward(self, x):
-        s = self.sigma.view(1,-1,1,1)
-        p = self.pow.view(1,-1,1,1)
-        w = self.weight.view(1,-1,1,1)
-        b = self.bias.view(1,-1,1,1)
-        xp = x ** p
-        # print(x.min(), x.max())
-        suppression_field = nn.functional.conv2d(xp, self.sum_kernel, groups=self.num_features)
-        # print(suppression_field.min(), suppression_field.max())
         hks = self.kernel_size // 2
-        suppression_field = nn.functional.pad(suppression_field, (hks, hks, hks, hks), mode='replicate')
-        # print(suppression_field.min(), suppression_field.max())
-        den = s**p + suppression_field
-        # print(den.min(), den.max())
-        num = xp
+        # print(x.min(), x.max())
+        if self.center:
+            mean = nn.functional.conv2d(x, self.sum_kernel)
+            mean = nn.functional.pad(mean, (hks, hks, hks, hks), mode='replicate')
+            # print(mean.min(), mean.max())
+            x = x - mean
+        # print(x.min(), x.max())
+        num = x ** self.n
         # print(num.min(), num.max())
-        xnorm = w * num / den + b
-        # print(xnorm.min(), xnorm.max())
+
+        var = nn.functional.conv2d(x ** self.m, self.sum_kernel)
+        var = nn.functional.pad(var, (hks, hks, hks, hks), mode='replicate')
+        # print(var.min(), var.max())
+        den = (self.sigma + var)**self.p
+        # print(den.min(), den.max())
+        xnorm = num / den
+        if self.affine:
+            w = self.weight.view(1,-1,1,1)
+            b = self.bias.view(1,-1,1,1)
+            xnorm = w * xnorm + b
+        if (not torch.isfinite(xnorm).all()):
+            print(xnorm.min(), xnorm.max())
+            print(num.min(), num.max())
+            print(den.min(), den.max())
+            exit()
         return xnorm
+
+class BioNorm(AbstractModel):
+    @define(slots=False)
+    class ModelParams(BaseParameters):
+        num_features:int = None
+        kernel_size:int = None
+        affine:bool=True
+        m: int = 2
+        n: int = 1
+        p: int = 1/2
+        sigma: int = 1e-2
+        center: bool = False
+    
+    def __init__(self, params: ModelParams) -> None:
+        super().__init__(params)
+        self._make_network()
+    
+    def _make_network(self):
+        kwargs_to_exclude = set(['cls'])
+        kwargs = self.params.asdict(filter=lambda a,v: a.name not in kwargs_to_exclude)
+        self.bn = _BioNorm(**kwargs)
+    
+    def forward(self, x):
+        return self.bn(x)
+
+    def compute_loss(self, x, y, return_logits=True):
+        out = self.forward(x)
+        logits = out
+        loss = torch.zeros((x.shape[0],), dtype=x.dtype, device=x.device)
+        if return_logits:
+            return logits, loss
+        else:
+            return loss
 
 class BioNormWrapper(AbstractModel):
     @define(slots=False)
@@ -98,12 +146,103 @@ class BioNormWrapper(AbstractModel):
                         module = getattr(module, name)
                 else:
                     try:
-                        module[int(name)] = BioNorm(module[int(name)].num_features, self.params.kernel_size)
+                        module[int(name)] = _BioNorm(module[int(name)].num_features, self.params.kernel_size)
                     except:
-                        module = setattr(module, name, BioNorm(getattr(module,name).num_features, self.params.kernel_size))
+                        module = setattr(module, name, _BioNorm(getattr(module,name).num_features, self.params.kernel_size))
 
     def forward(self, *args, **kwargs):
         return self.wrapped_class(*args, **kwargs)
 
     def compute_loss(self, *args, **kwargs):
         return self.wrapped_class.compute_loss(*args, **kwargs)
+
+# class ConvBioNorm(nn.Module):
+#     def __init__(self, 
+#                  in_channels:int,
+#                  out_channels:int,
+#                  kernel_size_1:int,
+#                  kernel_size_2:int,
+#                  stride:int = 1,
+#                  padding:int = 0,
+#                  dilation:int = 1,
+#                  bias:bool = True,
+#                  n:int = 1,
+#                  m:int = 1,
+#                  p:int = 1):
+#         super().__init__()
+#         self.kernel_size_1 = kernel_size_1
+#         self.kernel_size_2 = kernel_size_2
+#         self.in_channels = in_channels
+#         self.out_channels = out_channels
+#         self.stride = stride
+#         self.padding = padding
+#         self.dilation = dilation
+#         self.n = n
+#         self.m = m
+#         self.p = p
+
+#         self.drive = nn.Conv2d(self.in_channels,
+#                                 self.out_channels,
+#                                 self.kernel_size_1,
+#                                 stride=self.stride,
+#                                 padding=self.padding,
+#                                 dilation=self.dilation,
+#                                 bias=self.bias)
+#         pad_offset = (self.kernel_size_2 - self.kernel_size_1) // 2
+#         self.norm = nn.Conv2d(self.in_channels,
+#                                 self.out_channels,
+#                                 self.kernel_size_2,
+#                                 stride=self.stride,
+#                                 padding=self.padding+pad_offset,
+#                                 dilation=self.dilation,
+#                                 bias=self.bias)
+#         nn.init.constant_(self.norm.bias, 1e-5)
+    
+#     def forward(self, x):
+#         drive = self.drive(x ** self.n)
+#         sup = self.norm(x ** self.m) ** self.p
+#         out = drive / sup
+#         return out
+    
+# class ConvBioNormWrapper(AbstractModel):
+#     @define(slots=False)
+#     class ModelParams(BaseParameters):
+#         wrapped_class_params: BaseParameters = None
+#         n:int = 1
+#         m:int = 1
+#         p:int = 1
+    
+#     def __init__(self, params: ModelParams) -> None:
+#         super().__init__(params)
+#         self.wrapped_class = params.wrapped_class_params.cls(params.wrapped_class_params)
+#         self._replace_batchnorm_with_bionorm()
+    
+#     def _replace_conv_with_conv_bionorm(self):
+#         batch_norm_paths = [n.split('.') for n,m in self.wrapped_class.named_modules() if isinstance(m, torch.nn.Conv2d)]
+#         for pth in batch_norm_paths:
+#             module = self.wrapped_class
+#             for j, name in enumerate(pth):
+#                 if j < len(pth)-1:
+#                     try:
+#                         module = module[int(name)]
+#                     except:
+#                         module = getattr(module, name)
+#                 else:
+#                     try:
+#                         conv = module[int(name)]
+#                         module[int(name)] = ConvBioNorm(
+#                             conv.in_channels,
+#                             conv.out_channels,
+#                             conv.kernel_size,
+#                             ,
+#                             stride=conv.stride,
+#                             padding=conv.padding
+#                         )
+#                     except:
+#                         module = setattr(module, name, BioNorm(getattr(module,name).num_features, self.params.kernel_size))
+
+#     def forward(self, *args, **kwargs):
+#         return self.wrapped_class(*args, **kwargs)
+
+#     def compute_loss(self, *args, **kwargs):
+#         return self.wrapped_class.compute_loss(*args, **kwargs)
--- src/bio_receptive_fields/bionorm_tasks.py
+++ src/bio_receptive_fields/bionorm_tasks.py
@@ -7,7 +7,7 @@ from mllib.optimizers.configs import (OneCycleLRConfig, SGDOptimizerConfig)
 from mllib.runners.configs import BaseExperimentConfig, TrainingParams
 from mllib.tasks.base_tasks import AbstractTask
 from adversarialML.biologically_inspired_models.src.task_utils import *
-from adversarialML.biologically_inspired_models.src.biological_normalization.bionorm import BioNormWrapper
+from adversarialML.biologically_inspired_models.src.bio_receptive_fields.bionorm import BioNormWrapper, BioNorm
 
 class Ecoset10CyclicLRRandAugmentBioNormXResNet2x18(AbstractTask):
     imgs_size = 224
@@ -30,19 +30,59 @@ class Ecoset10CyclicLRRandAugmentBioNormXResNet2x18(AbstractTask):
         resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams(self.input_size, 1000), num_classes=1000,
                                             normalization_layer_params=NormalizationLayer.get_params(),
                                             widen_factor=self.widen_factor)
-        p = BioNormWrapper.ModelParams(BioNormWrapper, resnet_p, 7)
+        p = BioNormWrapper.ModelParams(BioNormWrapper, resnet_p, 5)
         return p
 
     def get_experiment_params(self) -> BaseExperimentConfig:
         nepochs = 60
         return BaseExperimentConfig(
             LightningAdversarialTrainer.TrainerParams(LightningAdversarialTrainer,
-                TrainingParams(logdir=LOGDIR, nepochs=nepochs, early_stop_patience=50, tracked_metric='val_accuracy',
+               TrainingParams(logdir=LOGDIR, nepochs=nepochs, early_stop_patience=50, tracked_metric='val_accuracy',
                     tracking_mode='max', scheduler_step_after_epoch=False
                 )
             ),
             SGDOptimizerConfig(lr=0.2, weight_decay=5e-4, momentum=0.9, nesterov=True),
-            OneCycleLRConfig(max_lr=0.4, epochs=nepochs, steps_per_epoch=376, pct_start=0.2, anneal_strategy='linear'),
+            OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=375, pct_start=0.1, anneal_strategy='linear'),
+            logdir=LOGDIR, batch_size=32
+        )
+
+class Ecoset10InputBioNormCyclicLRRandAugmentBioNormXResNet2x18(AbstractTask):
+    imgs_size = 224
+    input_size = [3, imgs_size, imgs_size]
+    widen_factor = 2
+    def get_dataset_params(self) :
+        p = get_ecoset10_params(train_transforms=[
+                torchvision.transforms.Resize(self.imgs_size),
+                torchvision.transforms.RandomCrop(self.imgs_size),
+                torchvision.transforms.RandomHorizontalFlip(),
+                torchvision.transforms.RandAugment(magnitude=15)
+            ],
+            test_transforms=[
+                torchvision.transforms.Resize(self.imgs_size),
+                torchvision.transforms.CenterCrop(self.imgs_size),
+            ])
+        return p
+
+    def get_model_params(self):
+        resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams(self.input_size, 10), num_classes=10,
+                                            normalization_layer_params=BioNorm.ModelParams(BioNorm, 3, 7, affine=False,
+                                                                                           center=True, channel_pooling=False,
+                                                                                           pad_before_sum=True),
+                                            widen_factor=self.widen_factor)
+        p = BioNormWrapper.ModelParams(BioNormWrapper, resnet_p,
+                                       BioNorm.ModelParams(BioNorm, kernel_size=5, center=True))
+        return p
+
+    def get_experiment_params(self) -> BaseExperimentConfig:
+        nepochs = 60
+        return BaseExperimentConfig(
+            LightningAdversarialTrainer.TrainerParams(LightningAdversarialTrainer,
+               TrainingParams(logdir=LOGDIR, nepochs=nepochs, early_stop_patience=50, tracked_metric='val_accuracy',
+                    tracking_mode='max', scheduler_step_after_epoch=False
+                )
+            ),
+            SGDOptimizerConfig(lr=0.2, weight_decay=5e-4, momentum=0.9, nesterov=True),
+            OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=375, pct_start=0.1, anneal_strategy='linear'),
             logdir=LOGDIR, batch_size=32
         )
 
@@ -68,9 +108,9 @@ class ImagenetCyclicLRRandAugmentBioNormXResNet2x18(AbstractTask):
 
     def get_model_params(self):
         resnet_p = XResNet18.ModelParams(XResNet18, CommonModelParams(self.input_size, 1000), num_classes=1000,
-                                            normalization_layer_params=NormalizationLayer.get_params(),
+                                            normalization_layer_params=BioNorm.ModelParams(BioNorm, 3, 3, affine=False, center=True, pad=True),
                                             widen_factor=self.widen_factor)
-        p = BioNormWrapper.ModelParams(BioNormWrapper, resnet_p, 7)
+        p = BioNormWrapper.ModelParams(BioNormWrapper, resnet_p, 5)
         return p
 
     def get_experiment_params(self) -> BaseExperimentConfig:
@@ -84,6 +124,6 @@ class ImagenetCyclicLRRandAugmentBioNormXResNet2x18(AbstractTask):
             SGDOptimizerConfig(lr=0.2, weight_decay=5e-4, momentum=0.9, nesterov=True),
             # OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=1839, pct_start=0.05, anneal_strategy='linear'),
             # OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=5632, pct_start=0.1, anneal_strategy='linear'),
-            OneCycleLRConfig(max_lr=0.2, epochs=nepochs, steps_per_epoch=4916, pct_start=0.2, anneal_strategy='linear'),
-            logdir=LOGDIR, batch_size=64 # 4GPUS 256 batch
+            OneCycleLRConfig(max_lr=0.1, epochs=nepochs, steps_per_epoch=4916, pct_start=0.1, anneal_strategy='cos', div_factor=10, three_phase=True),
+            logdir=LOGDIR, batch_size=64
         )
